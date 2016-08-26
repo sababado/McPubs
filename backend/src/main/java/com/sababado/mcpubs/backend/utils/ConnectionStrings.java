6f1fd859008b130655d01558287b74d612551056
Robert Szabo <robert.james.szabo@gmail.com>
2016-08-25 23:02:53 -0400

    Adding pub records works.

--- backend/src/main/java/com/sababado/mcpubs/backend/utils/ConnectionStrings.java
+++ backend/src/main/java/com/sababado/mcpubs/backend/utils/ConnectionStrings.java
@@ -4,6 +4,6 @@ package com.sababado.mcpubs.backend.utils;
  * Created by robert on 8/25/16.
  */
 public class ConnectionStrings {
-    public static final String PROD_DB = "jdbc:google:mysql://voltaic-flag-141523:us-central1:mcpubs?user=root";
-    public static final String DEV_DB = "jdbc:mysql://127.0.0.1:8889/mcPubsDev?user=root";
+    public static final String PROD_DB = "USE_YOUR_OWN_PROD_DB";
+    public static final String DEV_DB = "USE_YOUR_OWN_DEV_DB";
 }
