fe98d976f064a13fce0fa7e2f47beca5662009a6
Robert Szabo <robert.james.szabo@gmail.com>
2016-08-28 13:24:31 -0400

    Consolidating unnecessary class.

--- backend/src/main/java/com/sababado/mcpubs/backend/utils/StringUtils.java
+++ backend/src/main/java/com/sababado/mcpubs/backend/utils/StringUtils.java
@@ -5,6 +5,9 @@ package com.sababado.mcpubs.backend.utils;
  */
 public class StringUtils {
 
+    public static final String PROD_DB = "jdbc:google:mysql://voltaic-flag-141523:us-central1:mcpubs?user=root";
+    public static final String DEV_DB = "jdbc:mysql://127.0.0.1:8889/mcPubsDev?user=root";
+
     /**
      * Use this regex to find one of any letter.
      */
