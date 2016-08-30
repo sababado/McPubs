de97136352ce3f40667bcc90eebbaaefbed3d1ba
Robert Szabo <robert.james.szabo@gmail.com>
2016-08-30 10:01:09 -0400

    Completing basic implementation of pubs in the list

--- app/src/main/java/com/sababado/mcpubs/ui/MyPubs.java
+++ app/src/main/java/com/sababado/mcpubs/ui/MyPubs.java
@@ -1,13 +1,19 @@
 package com.sababado.mcpubs.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.sababado.mcpubs.R;
 import com.sababado.mcpubs.models.Pub;
@@ -25,8 +31,7 @@ public class MyPubs extends AppCompatActivity implements MyPubsFragment.Callback
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                newPub();
             }
         });
     }
@@ -55,10 +60,49 @@ public class MyPubs extends AppCompatActivity implements MyPubsFragment.Callback
 
     @Override
     public void editPub(Pub pub, int index) {
-        // TODO start dialog with pub
+        startPubDialog(pub, index, R.string.edit_pub);
     }
 
     public void newPub() {
-        // TODO start dialog with pub
+        startPubDialog(null, -1, R.string.new_pub);
+    }
+
+    public void startPubDialog(final Pub pub, final int index, @StringRes int title) {
+        final EditText tv = new EditText(this);
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setCancelable(true)
+                .setNegativeButton(R.string.cancel, null)
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Pub savedPub = pub == null ? new Pub() : pub;
+                        String savedTitle = tv.getText().toString().trim();
+                        // Only save changes if there are changes to save, or if editing a pub and
+                        // the title has changed.
+                        if (!TextUtils.isEmpty(savedTitle) && !TextUtils.equals(pub == null ? "" : pub.getTitle(), savedTitle)) {
+                            savedPub.setTitle(savedTitle);
+                            pushPubUpdate(savedPub, index);
+                        }
+                    }
+                })
+                .create();
+        if (pub != null) {
+            tv.setText(pub.getTitle());
+        }
+        alertDialog.setView(tv);
+        alertDialog.show();
+    }
+
+    private void pushPubUpdate(Pub pub, int index) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MyPubsFragment fragment = (MyPubsFragment) fragmentManager.findFragmentById(R.id.my_pubs_fragment);
+        if (!fragment.isRemoving() && fragment.isVisible()) {
+            if (index > 0) {
+                fragment.editPub(index, pub);
+            } else {
+                fragment.addPub(pub);
+            }
+        }
     }
 }
--- app/src/main/java/com/sababado/mcpubs/ui/MyPubsFragment.java
+++ app/src/main/java/com/sababado/mcpubs/ui/MyPubsFragment.java
@@ -1,10 +1,15 @@
 package com.sababado.mcpubs.ui;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.sababado.mcpubs.PubAdapter;
@@ -53,6 +58,7 @@ public class MyPubsFragment extends ListFragment {
             pubAdapter = new PubAdapter(getContext(), pubList);
         }
         setListAdapter(pubAdapter);
+        registerForContextMenu(getListView());
     }
 
     @Override
@@ -76,6 +82,51 @@ public class MyPubsFragment extends ListFragment {
         pubAdapter.notifyDataSetChanged();
     }
 
+    public void deletePub(int index) {
+        pubList.remove(index);
+        pubAdapter.setData(pubList);
+        pubAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.pub_list_item_action_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final int position = info.position;
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                Pub clickedPub = pubAdapter.getItem(position);
+                if (callbacks != null) {
+                    callbacks.editPub(clickedPub, position);
+                }
+                break;
+            case R.id.action_delete:
+                new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.delete_pub)
+                        .setMessage(R.string.are_you_sure_delete_pub)
+                        .setNegativeButton(R.string.cancel, null)
+                        .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                deletePub(position);
+                            }
+                        }).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onDestroyView() {
+        unregisterForContextMenu(getListView());
+        super.onDestroyView();
+    }
+
     @Override
     public void onDetach() {
         callbacks = null;
@@ -84,6 +135,7 @@ public class MyPubsFragment extends ListFragment {
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
+        getListView().showContextMenu();
         Pub clickedPub = pubAdapter.getItem(position);
         if (callbacks != null) {
             callbacks.editPub(clickedPub, position);
