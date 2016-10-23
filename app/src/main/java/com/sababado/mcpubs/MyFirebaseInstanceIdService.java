d07b79c64e1ff790e11d6fb6a9c549190c9c77a0
Robert Szabo <robert.james.szabo@gmail.com>
2016-10-23 17:25:17 -0400

    Signing configs.

--- app/src/main/java/com/sababado/mcpubs/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/sababado/mcpubs/MyFirebaseInstanceIdService.java
@@ -1,7 +1,6 @@
 package com.sababado.mcpubs;
 
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
@@ -19,7 +18,7 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+//        Log.d(TAG, "Refreshed token: " + refreshedToken);
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
@@ -33,7 +32,7 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
         sharedPreferences.edit()
                 .putString(Utils.DEVICE_TOKEN, newToken)
                 .apply();
-        Log.d(TAG, "registering device: " + oldToken + " / " + newToken);
+//        Log.d(TAG, "registering device: " + oldToken + " / " + newToken);
         if (oldToken == null) {
             oldToken = "";
         }
@@ -42,7 +41,7 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
                     .execute();
             Utils.setMetaData(this, Utils.LAST_KEEP_ALIVE, System.currentTimeMillis());
         } catch (IOException e) {
-            Log.e(TAG, "Problem with device registration: " + e.getMessage());
+//            Log.e(TAG, "Problem with device registration: " + e.getMessage());
         }
     }
 }
